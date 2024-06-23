58c3aa94b651e0ac39f032f632c57e6fee7cef53
Ian MacDonald <ianmacdonaldm@gmail.com>
2024-06-23 00:47:30 -0400

    Added automatic SCSS token inclusion

--- tsup.config.ts
+++ tsup.config.ts
@@ -1,3 +1,4 @@
+import path from 'path';
 import { copy } from 'esbuild-plugin-copy';
 import svgr from 'esbuild-plugin-svgr';
 import { postcssModules, sassPlugin } from 'esbuild-sass-plugin';
@@ -18,14 +19,23 @@ export default defineConfig({
     esbuildPlugins: [
         sassPlugin({
             type: 'style',
+            precompile(source, pathname) {
+                const filePath = pathname.split('/').slice(-1)[0];
+                if (!filePath.startsWith('_')) {
+                    const prepend = `@use "${path.resolve(__dirname, './src/styles')}" as *;`;
+                    return prepend + '\n' + source;
+                } else {
+                    return source;
+                }
+            },
             transform: postcssModules({})
         }),
         svgr(),
         copy({
             resolveFrom: 'cwd',
             assets: {
-                from: ['./src/style/**'],
-                to: ['./lib/style/']
+                from: ['./src/styles/**'],
+                to: ['./lib/styles/']
             },
             watch: true
         })
