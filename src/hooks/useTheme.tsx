29789e340c8df20199ff0ea0a16ab339430c6c1f
Ian MacDonald <ianmacdonaldm@gmail.com>
2025-11-04 18:32:23 -0800

    Fixed theming in sandboxed environments

--- src/hooks/useTheme.tsx
+++ src/hooks/useTheme.tsx
@@ -3,13 +3,22 @@ import { useEffect, useState } from 'react';
 
 const useTheme = (initialTheme: Theme = 'light', serialize: boolean = true): [Theme, SetState<Theme>] => {
     const [theme, setTheme] = useState<Theme>(() => {
-        const savedTheme = serialize ? (localStorage.getItem('theme') as Theme) : null;
-        return savedTheme || initialTheme;
+        if (!serialize) return initialTheme;
+        try {
+            const saved = localStorage.getItem('theme');
+            return (saved as Theme) || initialTheme;
+        } catch {
+            return initialTheme;
+        }
     });
 
     useEffect(() => {
-        if (serialize) {
-            localStorage.setItem('theme', theme);
+        try {
+            if (serialize) {
+                localStorage.setItem('theme', theme);
+            }
+        } catch {
+            // No local serialization
         }
         document.documentElement.setAttribute('data-theme', theme);
     }, [theme]);
